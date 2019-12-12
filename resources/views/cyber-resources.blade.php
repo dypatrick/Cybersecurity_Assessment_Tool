1285896fe8d64f03500163a96bb3f932971fe11e
Daniel Patrick <dyp400@gmail.com>
2019-12-12 13:07:02 -0500

    Update

--- resources/views/cyber-resources.blade.php
+++ resources/views/cyber-resources.blade.php
@@ -79,7 +79,8 @@
                 font-size: 84px;                 
                 text-align: center;                 
                 color:green;                 
-                margin-top: -350px;     
+                margin-top: -225px; 
+                position: absolute;    
             } 
  
             .start             
@@ -93,6 +94,28 @@
                 position: absolute;                 
                 left: 20px; 
             } 
+            .physical
+            {
+                font-size: 30px;                 
+                font-weight: bold;                 
+                text-align: left;                 
+                position: inline;                 
+                margin-top: 50px;                 
+                margin-left: -20px;                 
+                position: relative;                 
+                left: 20px;
+            }
+            .device
+            {
+                font-size: 30px;                 
+                font-weight: bold;                 
+                text-align: left;                 
+                position: inline;                 
+                margin-top: 45px;                 
+                margin-left: -20px;                 
+                position: relative;                 
+                left: 20px;
+            }
 
             .download 
             { 
@@ -110,7 +133,7 @@
                 font-size: 20px;                 
                 text-align: left;                 
                 position: inline;                 
-                margin-top: -50px;                 
+                margin-top: 100px;                 
                 margin-left: 50px;                 
                 position: absolute;                 
                 left: 20px; 
@@ -121,10 +144,18 @@
                 text-align: left;                 
                 position: inline;                 
                 margin-top: -120px;                 
-                margin-right: -845px;                 
-                position: relative;                 
-                word-wrap: break-word;                 
-                width: 1050px; 
+                margin-right: -605px;                 
+                position: relative;                                 
+            } 
+            .social
+            { 
+                font-size: 30px;                 
+                text-align: middle;  
+                font-weight: bold;               
+                position: inline;                 
+                margin-top: 20px;                 
+                margin-left: 0px;                 
+                position: absolute;                                 
             } 
             .text-center 
             { 
@@ -173,13 +204,34 @@
  
                 <div class="start-description"> 
                     <a style="color:green;" href="https://cloud.google.com/solutions/modern-password-security-for-users.pdf ">Modern Password Security for Users</a><br> 
-                    <a style="color:green;" href="https://www.ieee-security.org/TC/SP2017/papers/207.pdf ">IEEE Security</a><br>  
+                    <a style="color:green;" href="https://www.ieee-security.org/TC/SP2017/papers/207.pdf ">IEEE Security</a><br> 
+                    <div class= "physical">
+                        Physical and Software Security
+                        <br>
+                    </div>
+                    <a style="color:green;" href="https://people.scs.carleton.ca/~paulv/papers/softprot8a.pdf">Software Protection and Application Security</a><br> 
+                    <a style="color:green;" href="https://nces.ed.gov/pubs98/safetech/chapter5.asp">Protecting Your System</a><br> 
+
+                    <div class= "device">
+                        Smart Device Security
+                        <br>
+                    </div>
+                    <a style="color:green;" href="https://arxiv.org/pdf/1903.05362.pdf">Security, Privacy, and Trust for Smart Devices</a><br> 
+                    <a style="color:green;" href="https://www.huawei.com/minisite/iot/img/iot_security_white_paper_2018_v2_en.pdf">IoT Security White Pages</a><br> 
+
                 </div> 
  
                 <div class="download-description"> 
                     <br> 
-                    <a style="color:green;"href="https://www.pacnog.org/pacnog10/track3/Security-Part-1.pdf">Internet and Network Security Fundamentals</a><br> 
+                    <a style="color:green;"href="https://arxiv.org/pdf/1903.05362.pdf">Internet and Network Security Fundamentals</a><br> 
                     <a style="color:green;"href="http://index-of.co.uk/Hacking-Coleccion/34%20-%20Computer,%20Network%20&%20Internet%20Security%20%5B-PUNISHER-%5D.pdf">An Introduction to Security</a><br>
+                    <div class= "social">
+                        Social Media
+                        <br>
+                        <a style="color:green; font-size: 20px; font-weight: normal;"href="http://www.ijsrp.org/research-paper-0115/ijsrp-p3755.pdf">How much Privacy we still have on Social Network</a><br> 
+                        <a style="color:green; font-size: 20px; font-weight: normal;"href="https://www.symantec.com/content/en/us/enterprise/media/security_response/whitepapers/the_risks_of_social_networking.pdf">The Risk of Social Networking</a><br> 
+                    </div>
+                    
                 </div> 
  
                 <div class="startlinks"> 
